c0c74066909775eeb5a270efcbf0451dd7cca96a
erdenetsogt <a.erdenetsogt@gmail.com>
2019-06-24 13:28:07 +0800

    add category

--- src/main/java/com/example/demo/controller/admin/CategoryApiController.java
+++ src/main/java/com/example/demo/controller/admin/CategoryApiController.java
@@ -1,10 +1,14 @@
 package com.example.demo.controller.admin;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.domain.Category;
+import com.example.demo.model.CategoryForm;
 import com.example.demo.repository.CategoryRepository;
 
 @RestController
@@ -18,4 +22,12 @@ public class CategoryApiController {
 		return repo.findAll();		
 	}
 		
+	@PostMapping("/api/category/save")	
+	@ResponseStatus(code = HttpStatus.NO_CONTENT)
+	public void save(CategoryForm form) {			
+		Category category = new Category();		
+		category.setName(form.getNer());
+		category.setDescription(form.getTailbar());		
+		repo.save(category);			
+	}
 }
